b16ecad37d17f7d8909534c56c3cb64f8eee577a
Hariharan Ravi <harihr2121@gmail.com>
2020-10-23 14:28:47 +0530

    heroku

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginComponent from './LoginCompoent';
+
+
+function App() {
+  return (
+    <div>
+      <LoginComponent/>
+    </div>
+  );
+}
+
+export default App;
